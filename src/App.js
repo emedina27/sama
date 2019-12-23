7498dd6075b60b4e2589a11784aa79ffe371cb26
manny <emmanuel.medina08@gmail.com>
2019-12-23 11:08:39 -0500

    responsive grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import InfoCard from "./Components/InfoCard";
+import GridLayout from "./Components/GridLayout";
 
 function App() {
   return (
     <div className="App">
-      <InfoCard />
+      <GridLayout />
     </div>
   );
 }
